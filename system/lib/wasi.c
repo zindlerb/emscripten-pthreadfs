3d1e2c3ee456a3549ef1225e8ecc48612bbcb545
Alon Zakai <azakai@google.com>
2019-08-27 16:48:25 -0700

    wip [ci skip]

--- system/lib/wasi.c
+++ system/lib/wasi.c
@@ -9,6 +9,7 @@
  * uses them, these stubs will throw at runtime.
  */
 
+#include <emscripten.h>
 #include <string.h>
 
 // libc
@@ -17,6 +18,10 @@ void exit(int status) {
   extern void __wasi_proc_exit(int);
   __wasi_proc_exit(status);
   __builtin_unreachable();
+}
+
+void abort() {
+  exit(1);
 }
 
 // Musl lock internals. As we assume wasi is single-threaded for now, these
@@ -44,4 +49,57 @@ void *emscripten_memcpy_big(void *restrict dest, const void *restrict src, size_
     n -= curr_n;
   }
   return dest;
+}
+
+// I/O syscalls - we support printf etc., but no filesystem access for now.
+
+static int* _vararg;
+
+static void set_vararg(int vararg) {
+  _vararg = (int*)vararg;
+}
+
+static int get_vararg_i32() {
+  return *vararg++;
+}
+
+int __syscall6(int id, int vararg) { // close
+  return 0;
+}
+
+int __syscall54(int id, int vararg) { // ioctl
+  EM_ASM({
+    console.log("syscall54:", $1);
+  }, vararg);
+  return 0;
+}
+
+int __syscall140(int id, int vararg) { // llseek
+  EM_ASM({
+    console.log("syscall140:", $1);
+  }, vararg);
+  return 0;
+}
+
+struct Iov {
+  unsigned char* ptr;
+  int len;
+};
+
+int __syscall146(int id, int vararg) { // writev
+  // hack to support printf, similar to library_syscalls.js handling of SYSCALLS_REQUIRE_FILESYSTEM=0
+  int stream = get_vararg_i32();
+  Iov* iov = (Iov*)get_vararg_i32();
+  int iovcnt = get_vararg_i32();
+  int ret = 0;
+  for (int i = 0; i < iovcnt; i++) {
+    int ptr = iov->ptr;
+    int len = iov->len;
+    iov++;
+    for (int j = 0; j < len; j++) {
+      SYSCALLS.printChar(stream, HEAPU8[ptr+j]);
+    }
+    ret += len;
+  }
+  return ret;
 }
