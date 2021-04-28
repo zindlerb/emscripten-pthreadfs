47902c3254f18be2158488e8092bdd311f9578f9
Sam Clegg <sbc@chromium.org>
2021-04-27 19:36:06 -0700

    Revert "Restore support for --cache command line flag (#13954)"
    This reverts commit a3609debc054d969ae5b1aae6897216728d73e52.

--- emar.py
+++ emar.py
@@ -6,19 +6,29 @@
 
 """Archive helper script
 
-This script acts as a frontend replacement for `llvm-ar`.
+This script acts as a frontend replacement for `ar`. See emcc.
+This is needed because, unlike a traditional linker, emscripten can't handle
+archive with duplicate member names.  This is because emscripten extracts
+archive to a temporary location and duplicate filenames will clobber each
+other in this case.
 """
+
+# TODO(sbc): Implement `ar x` within emscripten, in python, to avoid this issue
+# and delete this file.
 
 import sys
 
-from tools import shared, building
+from tools import shared
 
 
 #
 # Main run() function
 #
 def run():
-  return shared.run_process([shared.LLVM_AR] + building.get_command_with_possible_response_file(sys.argv[1:]), stdin=sys.stdin, check=False).returncode
+  # The wasm backend doesn't suffer from the same problem as fastcomp so it
+  # doesn't need the filename hashing.
+  cmd = [shared.LLVM_AR] + sys.argv[1:]
+  return shared.run_process(cmd, stdin=sys.stdin, check=False).returncode
 
 
 if __name__ == '__main__':
