23a7f622671a256e964f720cbaaeb5100bdcdc1b
Richard Stotz <richardstotz@google.com>
2021-10-01 15:52:04 +0200

    Fixed test formatting

--- pthreadfs/examples/emscripten-tests/filenames.cpp
+++ pthreadfs/examples/emscripten-tests/filenames.cpp
@@ -64,61 +64,61 @@ void test_file_contents(const char* path) {
 
 void test_readdir(const char* path) {
   int err;
-  DIR* dir;
-  struct dirent* ent;
+  DIR* directory_handle;
+  struct dirent* directory_entry;
   int i;
 
-  const char* filename = strrchr(path, '/')+1;
-  char * filename_lowercase = (char *)malloc(strlen(filename) + 1);
+  const char* filename = strrchr(path, '/') + 1;
+  char* filename_lowercase = (char*)malloc(strlen(filename) + 1);
   strcpy(filename_lowercase, filename);
-  for(int i = 0; filename_lowercase[i]; i++){
+  for (int i = 0; filename_lowercase[i]; i++) {
     filename_lowercase[i] = tolower(filename_lowercase[i]);
   }
 
   int length_of_path = filename - path;
-  char *folder = (char *) malloc(length_of_path + 1);
+  char* folder = (char*)malloc(length_of_path + 1);
   memcpy(folder, path, length_of_path);
   folder[length_of_path] = '\0';
 
   printf("Test readdir for path %s\n", path);
   create_file(path, /*buffer=*/path, 0666);
 
-
-  dir = opendir(folder);
-  assert(dir);
-  int seen[3] = {0, 0, 0};
+  directory_handle = opendir(folder);
+  assert(directory_handle);
+  int seen_files[3] = {0, 0, 0};
   for (i = 0; i < 3; i++) {
     errno = 0;
-    ent = readdir(dir);
-    assert(ent);
-    assert(ent->d_reclen == sizeof(*ent));
+    directory_entry = readdir(directory_handle);
+    assert(directory_entry);
+    assert(directory_entry->d_reclen == sizeof(*directory_entry));
     // Convert filenames to lowercase, since the system is supposed to be case-insensitive
-    for(int i = 0; ent->d_name[i]; i++){
-      ent->d_name[i] = tolower(ent->d_name[i]);
+    for (int i = 0; directory_entry->d_name[i]; i++) {
+      directory_entry->d_name[i] = tolower(directory_entry->d_name[i]);
     }
-    if (!seen[0] && !strcmp(ent->d_name, ".")) {
-      assert(ent->d_type & DT_DIR);
-      seen[0] = 1;
+    if (!seen_files[0] && !strcmp(directory_entry->d_name, ".")) {
+      assert(directory_entry->d_type & DT_DIR);
+      seen_files[0] = 1;
       continue;
     }
-    if (!seen[1] && !strcmp(ent->d_name, "..")) {
-      assert(ent->d_type & DT_DIR);
-      seen[1] = 1;
+    if (!seen_files[1] && !strcmp(directory_entry->d_name, "..")) {
+      assert(directory_entry->d_type & DT_DIR);
+      seen_files[1] = 1;
       continue;
     }
-    if (!seen[2] && !strcmp(ent->d_name, filename_lowercase)) {
-      assert(ent->d_type & DT_REG);
-      seen[2] = 1;
+    if (!seen_files[2] && !strcmp(directory_entry->d_name, filename_lowercase)) {
+      assert(directory_entry->d_type & DT_REG);
+      seen_files[2] = 1;
       continue;
     }
-    assert(0 && "odd filename");
+    assert(0 && "Found unexpected file in directory");
   }
-  ent = readdir(dir);
-  if (ent)
-    printf("surprising ent: %p : %s\n", ent, ent->d_name);
-  assert(!ent);
+  directory_entry = readdir(directory_handle);
+  if (directory_entry)
+    printf(
+      "Unexpected path in directory found: %p : %s\n", directory_entry, directory_entry->d_name);
+  assert(!directory_entry);
 
-  err = closedir(dir);
+  err = closedir(directory_handle);
   assert(!err);
 
   unlink(path);
@@ -129,17 +129,17 @@ void test_readdir(const char* path) {
 int main() {
   setup();
 
-  const char* paths[] = {"persistent/filenametest/file.txt", "persistent/filenametest/file with space",
-    "persistent/filenametest/hyphen-file", "persistent/filenametest/underscore_file", "persistent/filenametest/UPPERCASE",
-    "persistent/filenametest/mixedCASE", "persistent/filenametest/file!", "persistent/filenametest/file(parenthesis)",
-    "persistent/filenametest/fileumlautäöüëé", "persistent/folder space/file",
-    "persistent/folder_underscore/file"};
+  const char* paths[] = {"persistent/filenametest/file.txt",
+    "persistent/filenametest/file with space", "persistent/filenametest/hyphen-file",
+    "persistent/filenametest/underscore_file", "persistent/filenametest/UPPERCASE",
+    "persistent/filenametest/mixedCASE", "persistent/filenametest/file!",
+    "persistent/filenametest/file(parenthesis)", "persistent/filenametest/fileumlautäöüëé",
+    "persistent/folder space/file", "persistent/folder_underscore/file"};
 
-  for (size_t i = 0; i < sizeof(paths)/sizeof(paths[0]); i++) {
+  for (size_t i = 0; i < sizeof(paths) / sizeof(paths[0]); i++) {
     test_file_contents(paths[i]);
     test_readdir(paths[i]);
   }
-  // test_preexisting_files();
   cleanup();
 
   puts("success");
