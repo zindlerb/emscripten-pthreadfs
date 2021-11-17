0bcfb683ea7ceb40cd071b29b352aa18c8ad35a0
Richard Stotz <richardstotz@google.com>
2021-11-17 10:25:26 +0100

    Improved test

--- pthreadfs/examples/emscripten-tests/realpath.cpp
+++ pthreadfs/examples/emscripten-tests/realpath.cpp
@@ -11,45 +11,55 @@
 
 #include <emscripten.h>
 
-
-void create_file(const char *path, const char *buffer, int mode) {
-  int fd = open(path, O_CREAT | O_TRUNC | O_RDWR , mode);
+void create_file(const char* path, const char* buffer, int mode) {
+  int fd = open(path, O_CREAT | O_TRUNC | O_RDWR, mode);
   assert(fd >= 0);
 
   int err = write(fd, buffer, sizeof(char) * strlen(buffer));
-  assert(err ==  (sizeof(char) * strlen(buffer)));
+  assert(err == (sizeof(char) * strlen(buffer)));
 
   close(fd);
 }
 
 void setup() {
   mkdir("persistent/folder", 0777);
+  mkdir("nonpersistent", 0777);
   create_file("persistent/folder/file", "abcdef", 0777);
+  create_file("nonpersistent/file2", "ghijkl", 0777);
 }
 
 void cleanup() {
   unlink("persistent/folder/file");
+  unlink("nonpersistent/file2");
   rmdir("persistent/folder");
+  rmdir("nonpersistent");
+}
+
+bool test_path(const char* provided_path, const char* expected_path) {
+  char computed_path[PATH_MAX];
+  char* res = realpath(provided_path, computed_path);
+  int success = strcmp(expected_path, computed_path);
+  return res && success;
 }
 
 void test() {
-    char buf[PATH_MAX];
-    char *res = realpath("persistent/folder/../folder/./file", buf);
-    if (res) {
-        printf("The file's real path is at %s.\n", buf);
-    } else {
-        char* errStr = strerror(errno);
-        printf("error string: %s\n", errStr);
+  const char* provided_paths[] = {"persistent/folder/../folder/file",
+    "persistent/../persistent/folder/file", "persistent/folder/./file", "/persistent/folder/file"};
+  const char* expected_paths[] = {"persistent/folder/file", "persistent/folder/file",
+    "persistent/folder/file", "persistent/folder/file"};
+  for (int i = 0; i < sizeof(provided_paths) / sizeof(char*); i++) {
+    if (!test_path(provided_paths[i], expected_paths[i])) {
+      char* errStr = strerror(errno);
+      printf("Realpath failed for path %d: %s. Error string: %s\n", i, provided_paths[i], errStr);
+      return;
     }
+  }
+  puts("success");
 }
 
 int main() {
   setup();
   test();
   cleanup();
-  puts("success");
   return EXIT_SUCCESS;
 }
-
-
-
