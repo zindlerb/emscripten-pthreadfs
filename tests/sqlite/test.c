7a722dbce77bc0ad912dcd742efa0e3f34948dbc
Brian Zindler <zindlerb@gmail.com>
2022-04-12 01:29:28 -0700

    superhuman commands

--- tests/sqlite/test.c
+++ tests/sqlite/test.c
@@ -24,11 +24,33 @@ int main(){
   int rc;
   int i;
   const char *commands[] = {
-    "CREATE TABLE t1(a INTEGER, b INTEGER, c VARCHAR(100));",
-    "INSERT INTO t1 VALUES(1,13153,'thirteen thousand one hundred fifty three');",
-    "INSERT INTO t1 VALUES(1,987,'some other number');",
-    "SELECT count(*) FROM t1;",
-    "SELECT a, b, c FROM t1;",
+    "CREATE TABLE bulk_actions (id TEXT PRIMARY KEY NOT NULL, json TEXT NOT NULL)",
+    "CREATE TABLE calendar_accounts (email TEXT NOT NULL, sync_token TEXT, UNIQUE(email))",
+    "CREATE TABLE calendars (id TEXT NOT NULL, account TEXT NOT NULL, name TEXT NOT NULL, json TEXT, page_token TEXT, sync_token TEXT, last_request_at INTEGER, page_count INTEGER, UNIQUE(account, id))",
+    "CREATE TABLE contacts (id TEXT NOT NULL, name TEXT, json TEXT, email TEXT NOT NULL, score REAL, score_data TEXT, likely_non_person BOOL DEFAULT 0, UNIQUE(id, email))",
+    "CREATE TABLE events (calendar_id TEXT NOT NULL, google_id TEXT NOT NULL, ical_id TEXT, summary TEXT, start_time INTEGER NOT NULL, end_time INTEGER, all_day INTEGER NOT NULL, json TEXT, UNIQUE(calendar_id, google_id))",
+    "CREATE TABLE general (key TEXT PRIMARY KEY NOT NULL, json TEXT NOT NULL)",
+    "CREATE TABLE labels (id TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL, slug TEXT, type TEXT)",
+    "CREATE TABLE list_ids (list_id TEXT NOT NULL, thread_id TEXT NOT NULL, sort INTEGER, unique(list_id, thread_id))",
+    "CREATE TABLE lists (id TEXT PRIMARY KEY NOT NULL, page_token TEXT, min_sort int, delta_token TEXT, catch_up_last_modified_datetime INTEGER)",
+    "CREATE TABLE messages (id TEXT PRIMARY KEY NOT NULL, timestamp INTEGER, is_sent INTEGER, emails TEXT NOT NULL, json TEXT, thread_id TEXT)",
+    "CREATE TABLE modifiers (id INTEGER PRIMARY KEY, name TEXT, queue_name TEXT NOT NULL, json TEXT NOT NULL, session_id TEXT, replace_id TEXT, created_at INTEGER, updated_at INTEGER, started_at INTEGER, completed_at INTEGER)",
+    "CREATE TABLE profiles (email TEXT PRIMARY KEY NOT NULL, insights TEXT, threads TEXT, twitter TEXT)",
+    "CREATE TABLE sync (id TEXT PRIMARY KEY NOT NULL, value TEXT)",
+    "CREATE TABLE threads (thread_id TEXT PRIMARY KEY NOT NULL, json TEXT, sort INTEGER, in_spam_trash BOOLEAN, has_attachments BOOLEAN, superhuman_data TEXT, needs_render INTEGER DEFAULT 1)",
+    "CREATE INDEX calendar_accounts_email ON calendar_accounts (email)",
+    "CREATE INDEX calendars_account_id ON calendars (account, id)",
+    "CREATE INDEX contacts_score ON contacts (score)",
+    "CREATE INDEX events_calendar_id_start_time_end_time ON events (calendar_id, start_time, end_time)",
+    "CREATE INDEX list_ids_list_id_sort ON list_ids (list_id, sort)",
+    "CREATE INDEX list_ids_thread_id ON list_ids (thread_id)",
+    "CREATE INDEX modifiers_completed_at ON modifiers (completed_at)",
+    "CREATE INDEX modifiers_queue_name ON modifiers (queue_name)",
+    "CREATE INDEX threads_needs_render ON threads (needs_render)",
+    "CREATE INDEX threads_sort ON threads (sort)",
+
+    "INSERT INTO labels VALUES(1,'test','test', 'test');",
+    "SELECT * FROM labels;",
     NULL
   };
 
@@ -49,4 +71,3 @@ int main(){
   sqlite3_close(db);
   return 0;
 }
-
